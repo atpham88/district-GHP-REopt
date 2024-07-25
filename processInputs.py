21fd19a130de3aaa699c36324eab0149f7a432dd
An Pham <An.Pham@nrel.gov>
2024-07-24 18:07:42 -0600

    add result summary

--- processInputs.py
+++ processInputs.py
@@ -12,14 +12,17 @@ data_path = os.path.join(dir, 'data')
 bau_scenario = pd.read_csv(os.path.join(data_path, "BAU_scenario.csv"))
 ghp_scenario = pd.read_excel(os.path.join(data_path, "GHP_scenario.xlsx"), sheet_name= "Timeseries")
 
-### Process buildings inputs
-## Site info
+### District inputs:
+## Shared GHX system
+number_of_boreholes = 65
+length_of_boreholes = 127.09
 
-# Number of buildings:
+## Site info
+# Number of buildings
 building_no = 2
 building_set = [x+1 for x in list(range(building_no))]
 
-# Location:
+# Location
 lon = -105.2648427
 lat = 39.99153232
 
@@ -32,17 +35,18 @@ for building in building_set:
     post["Site"]["longitude"] = lon
     
     # Load input:
-    building_elec_load_tot = ghp_scenario['heating_electric_power_'+str(building)] + ghp_scenario['pump_power_'+str(building)] + ghp_scenario['pump_power_'+str(building)]
+    building_elec_load_tot = ghp_scenario['heating_electric_power_'+str(building)]/1000 + ghp_scenario['pump_power_'+str(building)]/1000 + ghp_scenario['pump_power_'+str(building)]/1000
     ghp_scenario['electric_load_tot_'+str(building)] = building_elec_load_tot
     building_elec_load = ghp_scenario['electric_load_tot_'+str(building)]
+    ghx_pump_electric_con = ghp_scenario["electrical_power_consumed"]/1000
 
-    #building_spaceheating_load = ghp_scenario['electric_load_tot_'+str(building)]*0
-    #building_spaceheating_load = building_spaceheating_load*0.003412/1000
+    building_spaceheating_load = ghp_scenario['electric_load_tot_'+str(building)]
+    building_spaceheating_load = building_spaceheating_load*0.000003412/1000
 
-    #post["SpaceHeatingLoad"] = {}
-    #post["DomesticHotWaterLoad"] = {}
-    #post["SpaceHeatingLoad"]["fuel_loads_mmbtu_per_hour"] = list(building_spaceheating_load)
-    #post["DomesticHotWaterLoad"]["fuel_loads_mmbtu_per_hour"] = list(building_spaceheating_load*0)
+    post["SpaceHeatingLoad"] = {}
+    post["DomesticHotWaterLoad"] = {}
+    post["SpaceHeatingLoad"]["fuel_loads_mmbtu_per_hour"] = list(building_spaceheating_load)
+    post["DomesticHotWaterLoad"]["fuel_loads_mmbtu_per_hour"] = list(building_spaceheating_load*0)
 
     post["ElectricLoad"] = {}
     post["ElectricLoad"]["loads_kw"] = list(building_elec_load)
@@ -54,8 +58,7 @@ for building in building_set:
         r = json.load(handle)
     post["ElectricTariff"]["urdb_response"] = r
 
-    # Read GHP and GHX sizes:
-    # GHP and floor area:
+    # Read GHP and GHX outputs:
     if building == 1:
         ghp_size = 36
         floor_area = 31623
@@ -64,28 +67,41 @@ for building in building_set:
         floor_area = 8804
 
     post["GHP"] = {}  
+    post["GHP"]["require_ghp_purchase"] = 1
     post["GHP"]["building_sqft"] = floor_area
+    #post["GHP"]["installed_cost_building_hydronic_loop_per_sqft"] = 1.7
+    #post["GHP"]["installed_cost_ghx_per_ft"] = 0
+    #post["GHP"]["installed_cost_heatpump_per_ton"] = 1075.0
 
-    #post["ExistingBoiler"] = {}
-    #post["ExistingBoiler"]["fuel_cost_per_mmbtu"] = 13.5
+    post["ExistingBoiler"] = {}
+    post["ExistingBoiler"]["fuel_cost_per_mmbtu"] = 13.5
 
-    # GHX:
-    with open(os.path.join(data_path, "ghpghx_response.json"), 'rb') as handle:
-        ghpghx_output = json.load(handle)
-
-    ghx_pump_electric_con = ghp_scenario["electrical_power_consumed"]
-    ghpghx_output["outputs"]["yearly_ghx_pump_electric_consumption_series_kw"] = list(ghx_pump_electric_con*0)
+    # ghpghx_responses:
+    #with open(os.path.join(data_path, "ghpghx_response.json"), 'rb') as handle:
+    #    ghpghx_output = json.load(handle)
+    ghpghx_output = {}
+    ghpghx_output["outputs"] = {}
+    ghpghx_output["inputs"] = {}
+    
+    ghpghx_output["outputs"]["heat_pump_configuration"] = "WSHP"
+    ghpghx_output["outputs"]["yearly_ghx_pump_electric_consumption_series_kw"] = [0] * 8760
+    #ghpghx_output["outputs"]["yearly_heat_pump_eft_series_f"] = [0] * 8760
+    ghpghx_output["outputs"]["number_of_boreholes"] = number_of_boreholes
+    ghpghx_output["outputs"]["length_boreholes_ft"] = length_of_boreholes
+    ghpghx_output["outputs"]["peak_heating_heatpump_thermal_ton"] = ghp_size
+    ghpghx_output["outputs"]["peak_cooling_heatpump_thermal_ton"] = ghp_size
     ghpghx_output["outputs"]["peak_combined_heatpump_thermal_ton"] = ghp_size
-    ghpghx_output["outputs"]["number_of_boreholes"] = 0
-    ghpghx_output["outputs"]["length_boreholes_ft"] = 0
-
-    # Dispatch output:
+    ghpghx_output["outputs"]["yearly_total_electric_consumption_kwh"] = sum(building_elec_load)
+    ghpghx_output["outputs"]["yearly_total_electric_consumption_series_kw"] = list(building_elec_load)
     ghpghx_output["outputs"]["yearly_heating_heatpump_electric_consumption_series_kw"] = list(building_elec_load/2)
     ghpghx_output["outputs"]["yearly_cooling_heatpump_electric_consumption_series_kw"] = list(building_elec_load/2)
-    ghpghx_output["outputs"]["yearly_total_electric_consumption_series_kw"] = list(building_elec_load)
+    
+    # Set all inputs = 0:
+    ghpghx_output["inputs"]["heating_thermal_load_mmbtu_per_hr"] = list(building_spaceheating_load)
+    ghpghx_output["inputs"]["cooling_thermal_load_ton"] = [0] * 8760
 
-    ghpghx_output = [ghpghx_output, ghpghx_output]
-    post["GHP"]["ghpghx_responses"] = ghpghx_output
+    ghpghx_output_all = [ghpghx_output, ghpghx_output]
+    post["GHP"]["ghpghx_responses"] = ghpghx_output_all
 
     #ghpghx_response ={}
     #ghpghx_response["outputs"] = {}
@@ -109,27 +125,36 @@ post_dist["ElectricTariff"] = {}
 post_dist["ElectricTariff"]["urdb_response"] = r
 
 # Read GHX sizes:
-with open(os.path.join(data_path, "ghpghx_response.json"), 'rb') as handle:
-    ghpghx_output = json.load(handle)
+#with open(os.path.join(data_path, "ghpghx_response.json"), 'rb') as handle:
+#    ghpghx_output = json.load(handle)
+ghpghx_output = {}
+ghpghx_output["outputs"] = {}
+ghpghx_output["inputs"] = {}
 
-number_of_boreholes = 65
-length_of_boreholes = 127.09
+ghpghx_output["inputs"]["heating_thermal_load_mmbtu_per_hr"] = list(building_spaceheating_load)
+ghpghx_output["inputs"]["cooling_thermal_load_ton"] = [0] * 8760
+
 ghx_pump_electric_con = ghp_scenario["electrical_power_consumed"]
 ghpghx_output["outputs"]["yearly_ghx_pump_electric_consumption_series_kw"] = list(ghx_pump_electric_con)
 ghpghx_output["outputs"]["peak_combined_heatpump_thermal_ton"] = 0
 ghpghx_output["outputs"]["number_of_boreholes"] = number_of_boreholes
 ghpghx_output["outputs"]["length_boreholes_ft"] = length_of_boreholes
+ghpghx_output["outputs"]["heat_pump_configuration"] = "WSHP"
 
 post_dist["GHP"] = {}  
+post_dist["GHP"]["require_ghp_purchase"] = 1
 post_dist["GHP"]["building_sqft"] = 0
+#post_dist["GHP"]["installed_cost_building_hydronic_loop_per_sqft"] = 1.7
+#post_dist["GHP"]["installed_cost_ghx_per_ft"] = 14.0
+#post_dist["GHP"]["installed_cost_heatpump_per_ton"] = 1075.0
 
 # Dispatch output:
 ghpghx_output["outputs"]["yearly_heating_heatpump_electric_consumption_series_kw"] = list(building_elec_load*0)
 ghpghx_output["outputs"]["yearly_cooling_heatpump_electric_consumption_series_kw"] = list(building_elec_load*0)
 ghpghx_output["outputs"]["yearly_total_electric_consumption_series_kw"] = list(building_elec_load*0)
 
-ghpghx_output = [ghpghx_output, ghpghx_output]
-post_dist["GHP"]["ghpghx_responses"] = ghpghx_output
+ghpghx_output_all = [ghpghx_output, ghpghx_output]
+post_dist["GHP"]["ghpghx_responses"] = ghpghx_output_all
 
 with open(os.path.join(posts_path, 'GHP_district.json'), 'w') as handle:
     json.dump(post_dist, handle)  
--- processOutputs.py
+++ processOutputs.py
@@ -17,10 +17,10 @@ colHeaders_types = {
 
     # Main Results
     "Building Name": str,
-    "LCC BAU [$]": float,
+    #"LCC BAU [$]": float,
     "LCC [$]": float,
-    "NPV [$]": float,
-    "Analysis Period Elec Cost Savings [%]": float,
+    #"NPV [$]": float,
+    #"Analysis Period Elec Cost Savings [%]": float,
     "lifecycle capital costs": float,
 
     ## LCC Breakdown
@@ -30,31 +30,31 @@ colHeaders_types = {
     "lifecycle_emissions_cost_climate": float,
     "REopt Solver Seconds": float,
 
-    "Simple Payback [years]": float,
+    #"Simple Payback [years]": float,
     "Initial Capital Costs After Incentives [$]": float,
     
     "Annual Load [kWh]": float,
  
-    "Year One Bill_BAU [$]": float,
-    "Year One Bill_OPTIMIZED [$]": float,
-    "Year One Bill Savings [$]": float,
+    #"Year One Bill_BAU [$]": float,
+    "Year One Bill [$]": float,
+    #"Year One Bill Savings [$]": float,
 
-    "Analysis Period Emissions_BAU [tonnes CO2]": float,
-    "Analysis Period Emissions_OPTIMIZED [tonnes CO2]": float,
-    "Analysis Period Emissions Savings [tonnes CO2]": float,
-    "Analysis Period Emissions Savings [%]": float,
-    "Analysis Period Emissions Savings [%] - Check": float,
-    "Annual Emissions_BAU [tonnes CO2]": float,
-    "Annual Emissions_OPTIMIZED [tonnes CO2]": float,
-    "Annual Emissions Savings [tonnes CO2]": float,
-    "Avoided Health Damage Costs [$]": float
+    #"Analysis Period Emissions_BAU [tonnes CO2]": float,
+    "Analysis Period Emissions [tonnes CO2]": float,
+    #"Analysis Period Emissions Savings [tonnes CO2]": float,
+    #"Analysis Period Emissions Savings [%]": float,
+    #"Analysis Period Emissions Savings [%] - Check": float,
+    #"Annual Emissions_BAU [tonnes CO2]": float,
+    "Annual Emissions [tonnes CO2]": float,
+    #"Annual Emissions Savings [tonnes CO2]": float,
+    "Health Damage Costs [$]": float
     
 }
 colHeaders = [i for i in colHeaders_types.keys()]
 
 # Functions:
 def SaveOutputs(res, name, colHeaders):
-    building = name.split("building", 1)[1]
+    building = name.split(".", 1)[0]
 
     # Get saved response
     with open(os.path.join(results_path, name), 'rb') as handle:
@@ -67,10 +67,10 @@ def SaveOutputs(res, name, colHeaders):
 
     #capital costs
     res = np.append(res, "building"+building) # Building Name
-    res = np.append(res, r["Financial"]["lcc_bau"])  # "Analysis Period Elec Costs_BAU [$]", 
+    #res = np.append(res, r["Financial"]["lcc_bau"])  # "Analysis Period Elec Costs_BAU [$]", 
     res = np.append(res, r["Financial"]["lcc"])    # "Analysis Period Elec Costs_OPTIMIZED [$]",           
-    res = np.append(res, r["Financial"]["npv"])    # NPV,
-    res = np.append(res, ((r["Financial"]["npv"])/(r["Financial"]["lcc_bau"]))*100) # "Analysis Period Elec Cost Savings [%]"
+    #res = np.append(res, r["Financial"]["npv"])    # NPV,
+    #res = np.append(res, ((r["Financial"]["npv"])/(r["Financial"]["lcc_bau"]))*100) # "Analysis Period Elec Cost Savings [%]"
     res = np.append(res, fin["lifecycle_capital_costs"]) #  "lifecycle_generation_tech_capital_costs",
     res = np.append(res, fin["lifecycle_om_costs_after_tax"]) #  "lifecycle_om_costs_after_tax",
     res = np.append(res, fin["lifecycle_capital_costs_plus_om_after_tax"]) # "lifecycle_capital_costs_plus_om_after_tax"
@@ -78,26 +78,27 @@ def SaveOutputs(res, name, colHeaders):
     res = np.append(res, fin["lifecycle_emissions_cost_climate"]) #  "lifecycle_emissions_cost_climate"
     res = np.append(res, r["solver_seconds"])    # "REopt Solver Seconds"
 
-    res = np.append(res, r["Financial"]["simple_payback_years"]) # "Simple Payback [years]",
+    #res = np.append(res, r["Financial"]["simple_payback_years"]) # "Simple Payback [years]",
     res = np.append(res, r["Financial"]["initial_capital_costs_after_incentives"]) #capital costs
 
     res = np.append(res, np.sum(r["ElectricLoad"]["load_series_kw"]))  # "Annual Load [kWh]"
 
-    res = np.append(res, tariff["year_one_bill_before_tax_bau"])   # "Year One Bill_BAU [$]",
-    res = np.append(res, tariff["year_one_bill_before_tax"])    # "Year One Bill_OPTIMIZED [$]",
-    tot_sav = tariff["year_one_bill_before_tax_bau"]-tariff["year_one_bill_before_tax"]
-    res = np.append(res, tot_sav)    # "Year One Bill Savings [$]",
+    #res = np.append(res, tariff["year_one_bill_before_tax_bau"])   # "Year One Bill [$]",
+    res = np.append(res, tariff["year_one_bill_before_tax"])    # "Year One Bill [$]",
+    #tot_sav = tariff["year_one_bill_before_tax_bau"]-tariff["year_one_bill_before_tax"]
+    #res = np.append(res, tot_sav)    # "Year One Bill Savings [$]",
 
-    res = np.append(res, site["lifecycle_emissions_tonnes_CO2_bau"])    # "Analysis Period Emissions_BAU [tonnes CO2]",
-    res = np.append(res, site["lifecycle_emissions_tonnes_CO2"])    # "Analysis Period Emissions_OPTIMIZED [tonnes CO2]",
-    res = np.append(res, site["lifecycle_emissions_tonnes_CO2_bau"]-site["lifecycle_emissions_tonnes_CO2"])    # "Analysis Period Emissions Savings [tonnes CO2]",
-    res = np.append(res, (site["lifecycle_emissions_tonnes_CO2_bau"]-site["lifecycle_emissions_tonnes_CO2"])/site["lifecycle_emissions_tonnes_CO2_bau"]*100)    # "Analysis Period Emissions Savings [%]",
-    res = np.append(res, site["lifecycle_emissions_reduction_CO2_fraction"])  # "Analysis Period Emissions Savings [%] - Check"
-    res = np.append(res, site["annual_emissions_tonnes_CO2_bau"])    # "Year One Emissions_BAU [tonnes CO2]",
-    res = np.append(res, site["annual_emissions_tonnes_CO2"])    # "Year One Emissions_OPTIMIZED [tonnes CO2]",
-    res = np.append(res, site["annual_emissions_tonnes_CO2_bau"]-site["annual_emissions_tonnes_CO2"])    # "Year One Emissions Savings [tonnes CO2]",
-    res = np.append(res, r["Financial"]["lifecycle_emissions_cost_health_bau"]-r["Financial"]["lifecycle_emissions_cost_health"]) # "Avoided Health Damage Costs [$]"
-                
+    #res = np.append(res, site["lifecycle_emissions_tonnes_CO2_bau"])    # "Analysis Period Emissions_BAU [tonnes CO2]",
+    res = np.append(res, site["lifecycle_emissions_tonnes_CO2"])    # "Analysis Period Emissions[tonnes CO2]",
+    #res = np.append(res, site["lifecycle_emissions_tonnes_CO2_bau"]-site["lifecycle_emissions_tonnes_CO2"])    # "Analysis Period Emissions Savings [tonnes CO2]",
+    #res = np.append(res, (site["lifecycle_emissions_tonnes_CO2_bau"]-site["lifecycle_emissions_tonnes_CO2"])/site["lifecycle_emissions_tonnes_CO2_bau"]*100)    # "Analysis Period Emissions Savings [%]",
+    #res = np.append(res, site["lifecycle_emissions_reduction_CO2_fraction"])  # "Analysis Period Emissions Savings [%] - Check"
+    #res = np.append(res, site["annual_emissions_tonnes_CO2_bau"])    # "Year One Emissions_BAU [tonnes CO2]",
+    res = np.append(res, site["annual_emissions_tonnes_CO2"])    # "Year One Emissions [tonnes CO2]",
+    #res = np.append(res, site["annual_emissions_tonnes_CO2_bau"]-site["annual_emissions_tonnes_CO2"])    # "Year One Emissions Savings [tonnes CO2]",
+    #res = np.append(res, r["Financial"]["lifecycle_emissions_cost_health_bau"]-r["Financial"]["lifecycle_emissions_cost_health"]) # "Avoided Health Damage Costs [$]"
+    res = np.append(res, r["Financial"]["lifecycle_emissions_cost_health"]) # "Health Damage Costs [$]"
+            
     return res
 
 files = os.listdir(results_path)
@@ -123,10 +124,9 @@ for name in files:
     df.round(3).to_csv(os.path.join(dir, "results", "results_summary", "results_summary.csv"))
 
 # Compile all results into a spreadsheet
-list_of_columns = ["Building Name", "NPV [$]", "LCC BAU [$]", "LCC [$]", "NPV [$]", "lifecycle capital costs",
-                   "Avoided Health Damage Costs [$]", "lifecycle capital costs", "Annual Emissions Savings [tonnes CO2]"]
+list_of_columns = ["Building Name", "LCC [$]", "lifecycle capital costs", "Health Damage Costs [$]", "Annual Emissions Savings [tonnes CO2]"]
 
-excel_filename = "GHP_main_results.xlsx"
+excel_filename = "GHP_results.xlsx"
 sheet_name = "all_results"
 
 with pd.ExcelWriter(os.path.join(dir, "results",excel_filename), engine='xlsxwriter') as writer:
